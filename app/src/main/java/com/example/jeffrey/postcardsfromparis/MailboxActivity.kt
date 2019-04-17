f3da5e426bae5dfadbdc5b9ae92e5cc001db52d3
jsun454 <jsun46@umail.ucsb.edu>
2019-04-16 19:27:08 -0700

    Temporary implementation of user logging out for testing purposes

--- app/src/main/java/com/example/jeffrey/postcardsfromparis/MailboxActivity.kt
+++ app/src/main/java/com/example/jeffrey/postcardsfromparis/MailboxActivity.kt
@@ -1,18 +1,40 @@
 package com.example.jeffrey.postcardsfromparis
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
+import com.google.firebase.auth.FirebaseAuth
 
 class MailboxActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mailbox)
+
+        if(FirebaseAuth.getInstance().uid == null) {
+            val intent = Intent(this, AuthUserActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(intent)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_nav, menu)
         return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if(item?.itemId == R.id.menu_nav_new_postcard) {
+            FirebaseAuth.getInstance().signOut()
+            val intent = Intent(this, AuthUserActivity::class.java)
+            // TODO: delete above 2 lines, uncomment below 1 line
+            // val intent = Intent(this, NewPostcardActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(intent)
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
