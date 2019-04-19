a48aa60e9e50a3b4c7b80725444e9a05a4bd6f8a
jsun454 <jsun46@umail.ucsb.edu>
2019-04-18 19:06:08 -0700

    Implemented basic layout for creating new postcards

--- app/src/main/java/com/example/jeffrey/postcardsfromparis/NewPostcardActivity.kt
+++ app/src/main/java/com/example/jeffrey/postcardsfromparis/NewPostcardActivity.kt
@@ -8,5 +8,7 @@ class NewPostcardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_postcard)
+
+
     }
 }
