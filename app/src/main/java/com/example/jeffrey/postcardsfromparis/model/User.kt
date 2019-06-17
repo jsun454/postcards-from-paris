e33a68dbf410ceef806e2f3c690c6bc95fb65db1
jsun454 <jsun46@umail.ucsb.edu>
2019-06-16 19:07:21 -0700

    Added timestamp fields for last postcard sent and received

--- app/src/main/java/com/example/jeffrey/postcardsfromparis/model/User.kt
+++ app/src/main/java/com/example/jeffrey/postcardsfromparis/model/User.kt
@@ -4,6 +4,12 @@ import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-class User(val uid: String, var name: String, var imgUrl: String): Parcelable {
-    constructor(): this("", "", "")
+class User(
+    val uid: String,
+    var name: String,
+    var imgUrl: String,
+    var lastSent: Long,
+    var lastReceived: Long
+): Parcelable {
+    constructor(): this("", "", "", 0L, 0L)
 }
