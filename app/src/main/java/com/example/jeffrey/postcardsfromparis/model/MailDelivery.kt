5ba3566ee1bb5762452f0963e673e79b842a8781
jsun454 <jsun46@umail.ucsb.edu>
2019-08-14 01:19:30 -0700

    Implemented basic postcard distribution system (small scale)

--- app/src/main/java/com/example/jeffrey/postcardsfromparis/model/MailDelivery.kt
+++ app/src/main/java/com/example/jeffrey/postcardsfromparis/model/MailDelivery.kt
@@ -1,7 +1,7 @@
 package com.example.jeffrey.postcardsfromparis.model
 
 import android.util.Log
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
 import com.google.firebase.storage.FirebaseStorage
 import java.util.*
 
@@ -10,6 +10,7 @@ object MailDelivery {
     private const val WELCOME_IMAGE = "welcome.jpg"
     private const val PFP_PROFILE_PICTURE = "20190104_022430.jpg" // TODO: update this for final location name
     private const val PFP_LOCATION = "Santa Barbara"
+    private const val MAX_RECIPIENTS = 10
 
     private val TAG = MailDelivery::class.java.simpleName
 
@@ -31,8 +32,8 @@ object MailDelivery {
 
                         fbc.onCallback()
                     }
-                    .addOnFailureListener { e ->
-                        Log.e(TAG, "Failed to create welcome postcard")
+                    .addOnFailureListener {
+                        Log.e(TAG, "Failed to create welcome postcard: ${it.message}")
 
                         fbc.onCallback()
                     }
@@ -45,13 +46,70 @@ object MailDelivery {
     }
 
     fun distributePostcard(postcard: Postcard, fbc: FirebaseCallback) {
-        // TODO: send postcard to each user who hasn't received a postcard in X amount of time (6 hours)
-        // TODO: update lastreceived on each user
-        // TODO: call fbc.onCallback() at the end of each success/failure listener
-        // TODO: this won't work on a large scale but use for now
+        var numRecipients = 0
+        var success = false
+        var done = false
+
+        val ref = FirebaseDatabase.getInstance().getReference("users").orderByChild("lastReceived")
+            .limitToFirst(MAX_RECIPIENTS)
+        ref.addChildEventListener(object: ChildEventListener {
+            override fun onChildAdded(p0: DataSnapshot, p1: String?) {
+                if(done) return
+
+                ++numRecipients
+
+                val user = p0.getValue(User::class.java) ?: return
+                val uid = user.uid
+                if(uid == postcard.author.uid) {
+                    --numRecipients
+                    return
+                }
+
+                val cardPath = UUID.randomUUID().toString()
+                val cardRef = FirebaseDatabase.getInstance().getReference("postcards/$uid/$cardPath")
+                cardRef.setValue(postcard)
+                    .addOnSuccessListener {
+                        Log.i(TAG, "Successfully delivered postcard to user")
+
+                        --numRecipients
+                        success = true
+
+                        if(numRecipients == 0) {
+                            fbc.onCallback(success)
+                            ref.removeEventListener(this)
+                        }
+                    }
+                    .addOnFailureListener {
+                        Log.e(TAG, "Failed to deliver postcard to user")
+
+                        --numRecipients
+
+                        if(numRecipients == 0) {
+                            fbc.onCallback(success)
+                        }
+                    }
+            }
+
+            override fun onCancelled(p0: DatabaseError) {}
+            override fun onChildMoved(p0: DataSnapshot, p1: String?) {}
+            override fun onChildChanged(p0: DataSnapshot, p1: String?) {}
+            override fun onChildRemoved(p0: DataSnapshot) {}
+        })
+
+        ref.addListenerForSingleValueEvent(object: ValueEventListener {
+            override fun onDataChange(p0: DataSnapshot) {
+                if(numRecipients == 0) {
+                    done = true
+                    fbc.onCallback(success)
+                }
+            }
+
+            override fun onCancelled(p0: DatabaseError) {}
+        })
     }
 
     interface FirebaseCallback {
         fun onCallback()
+        fun onCallback(success: Boolean)
     }
 }
