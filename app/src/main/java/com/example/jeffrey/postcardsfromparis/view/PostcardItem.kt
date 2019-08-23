ec5f0b48354607bdc6e4be3d6f6712e909ce3391
jsun454 <jsun46@umail.ucsb.edu>
2019-08-23 12:46:53 -0700

    Added formatted timestamp to postcard

--- app/src/main/java/com/example/jeffrey/postcardsfromparis/view/PostcardItem.kt
+++ app/src/main/java/com/example/jeffrey/postcardsfromparis/view/PostcardItem.kt
@@ -8,6 +8,9 @@ import com.xwray.groupie.Item
 import com.xwray.groupie.ViewHolder
 import kotlinx.android.synthetic.main.partial_postcard_back.view.*
 import kotlinx.android.synthetic.main.partial_postcard_front.view.*
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.TimeUnit
 
 /**
  * This class provides the visual representation of a postcard
@@ -46,6 +49,7 @@ class PostcardItem(private val postcard: Postcard, var showFront: Boolean = true
                 // Bind author and postcard information to back
                 partial_postcard_back_txt_name.text = postcard.author.name
                 partial_postcard_back_txt_location.text = postcard.location
+                partial_postcard_back_txt_time.text = getTimeText(postcard.time)
 
                 if(postcard.author.imgUrl.isNotEmpty()) {
                     val profileUri = Uri.parse(postcard.author.imgUrl)
@@ -53,6 +57,43 @@ class PostcardItem(private val postcard: Postcard, var showFront: Boolean = true
                 }
 
                 partial_postcard_back_txt_postcard_message.text = postcard.message
+            }
+        }
+    }
+
+    /**
+     * Converts Unix time into a formatted timestamp
+     *
+     * @param ut Unix time in milliseconds
+     * @return the formatted timestamp as a string
+     */
+    private fun getTimeText(ut: Long): String {
+        val now = System.currentTimeMillis()
+        val elapsed = now - ut
+
+        // Time units in milliseconds
+        val minute = TimeUnit.MINUTES.toMillis(1)
+        val hour = TimeUnit.HOURS.toMillis(1)
+        val day = TimeUnit.DAYS.toMillis(1)
+        val month = TimeUnit.DAYS.toMillis(30)
+        val year = TimeUnit.DAYS.toMillis(365)
+
+        // Return the amount of time that has elapsed between now and the time that was passed in
+        return when(elapsed) {
+            in 0 until minute -> "Just now"
+            in minute until 2*minute -> "${elapsed / minute} minute ago"
+            in 2*minute until hour -> "${elapsed / minute} minutes ago"
+            in hour until 2*hour -> "${elapsed / hour} hour ago"
+            in 2*hour until day -> "${elapsed / hour} hours ago"
+            in day until 2*day -> "${elapsed / day} day ago"
+            in 2*day until month -> "${elapsed / day} days ago"
+            in month until 2*month -> "${elapsed / month} month ago"
+            in 2*month until year -> "${elapsed / month} months ago"
+            else -> {
+                // If more than a year has elapsed, display the passed-in time as a standalone date
+                val date = SimpleDateFormat("MMM d, yyyy", Locale.ENGLISH)
+                date.timeZone = TimeZone.getDefault()
+                return date.format(Date(ut))
             }
         }
     }
