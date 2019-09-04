c373d1b14efdf42d6a9c3224028eb4aa369df400
jsun454 <jsun46@umail.ucsb.edu>
2019-09-04 02:10:55 -0700

    Changed toast background color

--- app/src/main/java/com/example/jeffrey/postcardsfromparis/util/SharedUtil.kt
+++ app/src/main/java/com/example/jeffrey/postcardsfromparis/util/SharedUtil.kt
@@ -4,11 +4,14 @@ import android.app.Activity
 import android.support.v7.app.AlertDialog
 import android.content.Context
 import android.content.Intent
+import android.graphics.PorterDuff
 import android.net.Uri
+import android.support.v4.content.ContextCompat
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.ImageView
 import android.widget.Toast
+import com.example.jeffrey.postcardsfromparis.R
 import com.squareup.picasso.Picasso
 
 /**
@@ -72,14 +75,26 @@ object SharedUtil {
      *
      * @param message the message to show
      */
-    fun Context.toast(message: CharSequence) = Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    fun Context.toast(message: CharSequence) {
+        val toast = Toast.makeText(this, message, Toast.LENGTH_SHORT)
+        val view = toast.view
+        view.background.setColorFilter(ContextCompat.getColor(this, R.color.colorDefault),
+            PorterDuff.Mode.SRC_IN)
+        toast.show()
+    }
 
     /**
      * Makes a long toast [message]
      *
      * @param message the message to show
      */
-    fun Context.longToast(message: CharSequence) = Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+    fun Context.longToast(message: CharSequence) {
+        val toast = Toast.makeText(this, message, Toast.LENGTH_LONG)
+        val view = toast.view
+        view.background.setColorFilter(ContextCompat.getColor(this, R.color.colorDefault),
+            PorterDuff.Mode.SRC_IN)
+        toast.show()
+    }
 
     /**
      * Opens the soft keyboard
